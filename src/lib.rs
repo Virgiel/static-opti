0280bb90792e2749f454950127a53eca42d7fe92
Virgiel <>
2022-01-04 01:32:37 +0100

    Support runtime optimisation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-use std::path::PathBuf;
+use std::{
+    io::{BufWriter, Write},
+    path::{Path, PathBuf},
+};
 
+use brotli::CompressorWriter;
 use hashbrown::HashMap;
+use libdeflater::{CompressionLvl, Compressor};
 use memmap2::Mmap;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Item {
@@ -24,14 +30,23 @@ pub struct FilesService {
 }
 
 impl FilesService {
-    /// Create a file service match from an item and an accept encoding header value
-    pub fn new(static_dir: impl Into<PathBuf>) -> Self {
-        let dir = static_dir.into();
+    /// Create a file service from an already optimized dir
+    pub fn new(static_dir: impl AsRef<Path>) -> Self {
+        let dir = static_dir.as_ref();
         let bytes = std::fs::read(dir.join("report.json")).unwrap();
         let items: Vec<Item> = serde_json::from_slice(&bytes).unwrap();
+        Self::from_item_and_path(items, &dir.join("out.static"))
+    }
 
-        let mmap =
-            unsafe { Mmap::map(&std::fs::File::open(dir.join("out.static")).unwrap()).unwrap() };
+    /// Create and optimized file service at runtime
+    pub fn build(static_dir: impl AsRef<Path>, temp_file: impl AsRef<Path>) -> Self {
+        let out = temp_file.as_ref();
+        let items = compress_merge(static_dir, out);
+        Self::from_item_and_path(items, out)
+    }
+
+    fn from_item_and_path(items: Vec<Item>, path: &Path) -> Self {
+        let mmap = unsafe { Mmap::map(&std::fs::File::open(path).unwrap()).unwrap() };
         let mut map = HashMap::new();
         for item in items {
             map.insert(item.path.clone(), item);
@@ -90,3 +105,82 @@ fn match_encoding_tag<'a>(
     (None, &item.plain)
 }
 
+pub fn compress_merge(in_dir: impl AsRef<Path>, out_file: impl AsRef<Path>) -> Vec<Item> {
+    let mut entries = Vec::new();
+
+    let in_dir = in_dir.as_ref();
+    walk(in_dir, &mut entries);
+    // Parallel compression
+    let compressed: Vec<_> = entries
+        .par_iter()
+        .map(|p| {
+            // Read plain file
+            let plain = std::fs::read(&p).unwrap();
+            // Format path
+            let path = p
+                .strip_prefix(in_dir)
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string()
+                .replace("\\", "/");
+            // Gzip compress
+            let mut compressor = Compressor::new(CompressionLvl::best());
+            let max_size = compressor.gzip_compress_bound(plain.len());
+            let mut gzip = vec![0; max_size];
+            let gzip_size = compressor.gzip_compress(&plain, &mut gzip).unwrap();
+            gzip.resize(gzip_size, 0);
+            let gzip = (gzip.len() * 100 / plain.len() < 90).then(|| (etag(&gzip), gzip));
+
+            // Brotli compress
+            let mut brotli = Vec::new();
+            let mut writer = CompressorWriter::new(&mut brotli, 4096, 11, 24);
+            writer.write_all(&plain).unwrap();
+            writer.flush().unwrap();
+            writer.into_inner();
+            let brotli = (brotli.len() * 100 / plain.len() < 90).then(|| (etag(&brotli), brotli));
+
+            (path, (etag(&plain), plain), gzip, brotli)
+        })
+        .collect();
+
+    // Write
+    let mut writer = BufWriter::new(std::fs::File::create(out_file).unwrap());
+    let mut count = 0;
+    let mut append = |content: &[u8]| {
+        let start = count;
+        count += content.len();
+        writer.write_all(&content).unwrap();
+        (start as u64, content.len() as u32)
+    };
+    let items: Vec<_> = compressed
+        .into_iter()
+        .map(|(path, (etag, plain), gzip, brotli)| Item {
+            path,
+            plain: (etag, append(&plain)),
+            gzip: gzip.map(|(etag, gzip)| (etag, append(&gzip))),
+            brotli: brotli.map(|(etag, brotli)| (etag, append(&brotli))),
+        })
+        .collect();
+    writer.flush().unwrap();
+    items
+}
+
+/// Generate strong etag from bytes
+fn etag(bytes: &[u8]) -> String {
+    let hash = xxhash_rust::xxh3::xxh3_128(bytes);
+    base64::encode_config(hash.to_le_bytes(), base64::URL_SAFE_NO_PAD)
+}
+
+/// Recursive walk of any file in a directory
+fn walk(path: &Path, paths: &mut Vec<PathBuf>) {
+    for entry in std::fs::read_dir(path).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if entry.file_type().unwrap().is_file() {
+            paths.push(path);
+        } else {
+            walk(&path, paths);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-use std::{
-    fmt::{Display, Formatter},
-    io::{BufWriter, Write},
-    path::{Path, PathBuf},
-    sync::atomic::{AtomicUsize, Ordering},
-    time::Instant,
-};
+use std::{io::Write, path::PathBuf, time::Instant};
 
-use libdeflater::{CompressionLvl, Compressor};
 use mimalloc::MiMalloc;
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
-use static_opti::Item;
-use status_line::StatusLine;
+use static_opti::compress_merge;
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
@@ -21,70 +12,11 @@ fn main() {
     let in_dir = PathBuf::from(in_dir);
     let out_dir = std::env::args().nth(2).unwrap();
     let out_dir = PathBuf::from(out_dir);
-    let mut entries = Vec::new();
     std::fs::remove_dir_all(&out_dir).ok();
     std::fs::create_dir_all(&out_dir).unwrap();
 
-    walk(&in_dir, &mut entries);
-
-    // Parallel compression
-    let status = StatusLine::new(Progress("compression", AtomicUsize::new(0), entries.len()));
-    let compressed: Vec<_> = entries
-        .par_iter()
-        .map(|p| {
-            // Read plain file
-            let plain = std::fs::read(&p).unwrap();
-            // Format path
-            let path = p
-                .strip_prefix(&in_dir)
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_string()
-                .replace("\\", "/");
-            // Gzip compress
-            let mut compressor = Compressor::new(CompressionLvl::best());
-            let max_size = compressor.gzip_compress_bound(plain.len());
-            let mut gzip = vec![0; max_size];
-            let gzip_size = compressor.gzip_compress(&plain, &mut gzip).unwrap();
-            gzip.resize(gzip_size, 0);
-            let gzip = (gzip.len() * 100 / plain.len() < 90).then(|| (etag(&gzip), gzip));
-
-            // Brotli compress
-            let mut brotli = Vec::new();
-            let mut writer = brotli::CompressorWriter::new(&mut brotli, 4096, 11, 24);
-            writer.write_all(&plain).unwrap();
-            writer.flush().unwrap();
-            writer.into_inner();
-            let brotli = (brotli.len() * 100 / plain.len() < 90).then(|| (etag(&brotli), brotli));
-
-            // Increment status
-            status.increment();
-
-            (path, (etag(&plain), plain), gzip, brotli)
-        })
-        .collect();
-
-    // Write
-    let mut writer = BufWriter::new(std::fs::File::create(out_dir.join("out.static")).unwrap());
-    let mut count = 0;
-    let mut append = |content: &[u8]| {
-        let start = count;
-        count += content.len();
-        writer.write_all(&content).unwrap();
-        (start as u64, content.len() as u32)
-    };
-    let items: Vec<_> = compressed
-        .into_iter()
-        .map(|(path, (etag, plain), gzip, brotli)| Item {
-            path,
-            plain: (etag, append(&plain)),
-            gzip: gzip.map(|(etag, gzip)| (etag, append(&gzip))),
-            brotli: brotli.map(|(etag, brotli)| (etag, append(&brotli))),
-        })
-        .collect();
-    writer.flush().unwrap();
-
+    let out_file = out_dir.join("out.static");
+    let items = compress_merge(in_dir, &out_file);
     // Write report
     std::fs::write(
         out_dir.join("report.json"),
@@ -93,7 +25,6 @@ fn main() {
     .unwrap();
 
     // Print stats
-    drop(status);
     let max = items.iter().map(|t| t.path.len()).max().unwrap_or(0);
     let stdout = std::io::stdout();
     let mut stdout = stdout.lock();
@@ -106,7 +37,7 @@ fn main() {
         max + 26
     )
     .unwrap();
-    for item in items {
+    for item in &items {
         let plain = item.plain.1 .1;
         write!(
             &mut stdout,
@@ -116,12 +47,12 @@ fn main() {
             max
         )
         .unwrap();
-        for opt in [item.gzip, item.brotli] {
+        for opt in [&item.gzip, &item.brotli] {
             if let Some((_, (_, len))) = opt {
                 write!(
                     &mut stdout,
                     "{:>7} {}%  ",
-                    format_size(len as f32),
+                    format_size(*len as f32),
                     (len * 100 / plain),
                 )
                 .unwrap();
@@ -134,30 +65,11 @@ fn main() {
     writeln!(
         &mut stdout,
         "\n Optimized {} files to {} in {:?}",
-        entries.len(),
-        format_size(count as f32),
+        items.len(),
+        format_size(std::fs::metadata(out_file).unwrap().len() as f32),
         start.elapsed()
     )
     .unwrap();
-}
-
-/// Generate strong etag from bytes
-fn etag(bytes: &[u8]) -> String {
-    let hash = xxhash_rust::xxh3::xxh3_128(bytes);
-    base64::encode_config(hash.to_le_bytes(), base64::URL_SAFE_NO_PAD)
-}
-
-/// Recursive walk of any file in a directory
-fn walk(path: &Path, paths: &mut Vec<PathBuf>) {
-    for entry in std::fs::read_dir(path).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        if entry.file_type().unwrap().is_file() {
-            paths.push(path);
-        } else {
-            walk(&path, paths);
-        }
-    }
 }
 
 /// Format byte size in an human readable format
@@ -170,26 +82,4 @@ fn format_size(mut size: f32) -> String {
         }
     }
     return format!("{:.1}TB", size);
-}
-
-struct Progress(&'static str, AtomicUsize, usize);
-
-impl Progress {
-    fn increment(&self) {
-        self.1.fetch_add(1, Ordering::SeqCst);
-    }
-}
-
-impl Display for Progress {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let progress = self.1.load(Ordering::SeqCst);
-        write!(
-            f,
-            "{}: {}% {}/{}",
-            self.0,
-            progress * 100 / self.2,
-            progress,
-            self.2
-        )
-    }
 }
